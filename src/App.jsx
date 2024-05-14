6928c3221200ed7eec986a41c29f121bf3493e65
christiansanchez-SE <christiansanchez714@gmail.com>
2024-05-13 22:00:44 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <h1>todo list</h1>
+    </div>
+
+  )
+}
+
+export default App
